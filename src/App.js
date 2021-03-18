79c768da7198e8a677cc5d87b6919086206dfaab
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-03-18 13:19:57 +0200

    Added navigation and productgroups to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navigation from './components/navigation';
+import ProductGroups from './components/productGroups';
+
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      <ProductGroups />
     </div>
   );
 }
