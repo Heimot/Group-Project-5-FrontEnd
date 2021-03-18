3938218354b7844c332b23043e63bad7fcfd93cf
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-03-18 15:12:23 +0200

    Fixed a links

--- src/components/productGroups.js
+++ src/components/productGroups.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Dialog from './dialog';
 import "./productGroups.css";
 
 function ProductGroups() {
     const [isOpen, setisOpen] = useState(false);
     const [dialogData, setdialogData] = useState(null);
-
+    const [catalog, setcatalog] = useState("");
     let arr = [{ id: "1", ryhma: "Osat", underGroups: ["1", "2", "3"] }, { id: "1", ryhma: "Koneet", underGroups: ["4", "5", "6"] }, { id: "1", ryhma: "Lisätarvikkeet", underGroups: ["Hiiret", "Näppäimistöt"] }, { id: "1", ryhma: "Komponentit", underGroups: ["GPU", "CPU", "EMOLEVY", "GPU", "CPU", "EMOLEVY", "GPU", "CPU", "EMOLEVY", "GPU", "CPU", "EMOLEVY", "GPU", "CPU", "EMOLEVY", "GPU", "CPU", "EMOLEVY", "GPU", "CPU", "EMOLEVY", "GPU", "CPU", "EMOLEVY"] }, { id: "1", ryhma: "Näytöt" }];
 
     return (
@@ -15,7 +15,7 @@ function ProductGroups() {
                 <div className="groups">
                     {arr.map(item => {
                         return (
-                            <button onMouseEnter={() => setisOpen(true) + setdialogData(item.underGroups)} onMouseLeave={() => setisOpen(false)}  className="groupsButton" key={item.ryhma}>{item.ryhma}</button>
+                            <a href={`/catalog/${item.ryhma}`} onMouseEnter={() => setcatalog(item.ryhma) + setisOpen(true) + setdialogData(item.underGroups)} onMouseLeave={() => setisOpen(false)} className="groupsButton" key={item.ryhma}>{item.ryhma}</a>
                         )
                     })}
                 </div>
@@ -23,7 +23,7 @@ function ProductGroups() {
             <Dialog mouseLeft={() => setisOpen(false)} mouseOn={() => setisOpen(true)} isOpen={isOpen} onLoad={false} onClose={() => setisOpen(false)}>
                 <div className="subGroups">
                     {dialogData && dialogData.length ? dialogData.map(item => {
-                        return (<button className="subGroupCol">{item}</button>)
+                        return (<a href={`/catalog/${catalog}/${item}`} className="subGroupCol">{item}</a>)
                     }) : <div></div>}
                 </div>
             </Dialog>
