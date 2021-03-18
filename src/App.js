067672a448fc0054633a89ca50ffb70c2295eb81
Tommi Pikkupeura <n0pito00@students.oamk.fi>
2021-03-18 14:19:08 +0200

    typo kommentissa, bottomlinks app.js:aan

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navigation from './components/navigation';
 import ProductGroups from './components/productGroups';
 import Footer from './components/Footer'
+import BottomLinks from './components/BottomLinks'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <ProductGroups />
+      <BottomLinks />
       <Footer />
     </div>
   );
