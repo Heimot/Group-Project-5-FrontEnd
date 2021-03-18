f9c4d5a4a0867dc8d7497b5c1ef7a99c91fc56cd
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-03-18 15:13:05 +0200

    Merged app.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      <div>
         <ProductGroups />
         <ProductList />
-      </div>
       <Footer />
     </div>
   );
