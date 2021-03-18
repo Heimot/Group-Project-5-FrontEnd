59d2b624e8ac416bcab52e92b3f26992b34057eb
Juho Vikman <c0viju00@students.oamk.fi>
2021-03-18 15:27:54 +0200

    kortit tuotteisiin

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
 
 export default function ProductItem() {
   return (
-      <div>
-        <img src="https://bulma.io/images/placeholders/128x128.png" />
-    </div>
+    <Card style={{ width: '13rem' }}>
+    <Card.Img variant="top" src="https://bulma.io/images/placeholders/128x128.png" />
+    <Card.Body>
+      <Card.Text>
+        Tuoteen nimi
+      </Card.Text>
+      <Card.Title>45€</Card.Title>
+    </Card.Body>
+  </Card>
   );
 };
