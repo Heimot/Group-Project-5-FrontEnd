64a7896f2bb5d90574f34a38e373197f0aaf1b05
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-03-18 17:00:20 +0200

    Added some CSS for better design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import ProductGroups from './components/productGroups';
 import Footer from './components/Footer'
 import ProductList from './components/ProductList';
 import BottomLinks from './components/BottomLinks'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="middleSmol">
       <Navigation />
-      <ProductGroups />
-      <ProductList />
+      <div className="row">
+        <div className="col-md-3 col-lg-2">
+          <ProductGroups />
+        </div>
+        <div className="col-md-9 col-lg-10">
+          <ProductList />
+        </div>
+      </div>
       <BottomLinks />
       <Footer />
     </div>
