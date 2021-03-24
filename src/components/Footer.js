cd399b12945ff469b63dce41602769bb41548f47
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-03-24 19:05:22 +0200

    Fixed some padding problems

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import './footer.css'
 
 export default function Footer() {
    return (
-      <footer className="bottomi">
+      <footer className="bottomi container-fluid">
          <div className="row">
-            <div className="col text-center">
+         <div className="col text-center">
                <p>2021</p>
                <p>Ryhmä 5</p>
             </div>
