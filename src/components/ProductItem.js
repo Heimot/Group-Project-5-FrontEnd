7d9b96a61ce4d6264937750651781b1e60ae2109
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-03-24 19:03:49 +0200

    Added a nice onmouseenter effect

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button';
+
+import './productItem.css';
 
 export default function ProductItem(props) {
+  const [open, setOpen] = useState(false)
+
   return (
-    <Card style={{ width: '13rem' }}>
-    <Card.Img variant="top" src="https://bulma.io/images/placeholders/128x128.png" />
+    <Card className="productCard" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
+    <a href={`/product?productid=${props.product.id}&product=${props.product.tuote}`}><Card.Img variant="top" src="https://bulma.io/images/placeholders/128x128.png" /></a>
     <Card.Body>
       <Card.Text>
+        <a href={`/product?productid=${props.product.id}&product=${props.product.tuote}`}>
         {props.product.tuote}
+        </a>
       </Card.Text>
       <Card.Title>{props.product.hinta}</Card.Title>
     </Card.Body>
+    {open ? <Card className="moreData">
+        <Button className="addToCart">Lisää ostoskoriin</Button>
+        <Card.Text>
+          U CAN ADD MORE TEXT HERE
+        </Card.Text>
+      </Card> : null}
   </Card>
   );
 };
