1e771c55c4357e18528d7e06127acbab9e782ca8
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-03-24 19:05:31 +0200

    Added alt

--- src/components/BottomLinks.js
+++ src/components/BottomLinks.js
@@ -5,7 +5,7 @@ export default function BottomLinks() {
     return (
         <footer>
             <div className="btPic">
-                <img src="https://bulma.io/images/placeholders/128x128.png" />
+                <img alt="links" src="https://bulma.io/images/placeholders/128x128.png" />
             </div>
         </footer>
     )
