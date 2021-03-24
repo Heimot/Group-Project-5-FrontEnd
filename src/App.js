72d1ab64bb86f86048718b2b537d2b58889f4009
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-03-24 19:05:58 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from './components/navigation';
 import ProductGroups from './components/productGroups';
 import Footer from './components/Footer'
 import ProductList from './components/ProductList';
 import BottomLinks from './components/BottomLinks'
 import AdSlider from './components/ads';
+import ProductPage from './components/productPage';
 import './App.css'
 
 function App() {
   return (
-    <div className="middleSmol">
-      <Navigation />
-      <div className="row">
-        <div className="col-md-3 col-lg-2">
-          <ProductGroups />
-        </div>
-        <div className="col-md-9 col-lg-10">
-          <AdSlider />
-        </div>
+    <Router>
+      <div className="middleSmol container">
+        <Switch>
+          <Route path="/">
+            <Navigation />
+            <div className="row rowWidth">
+              <div className="col-md-3 col-lg-2 fucking">
+                <Route exact path="/">
+                  <ProductGroups />
+                </Route>
+              </div>
+              <div className="col-md-9 col-lg-10 fuck">
+                <Route exact path="/">
+                  <AdSlider />
+                </Route>
+              </div>
+            </div>
+            <Route path="/product">
+              <ProductPage />
+            </Route>
+            <Route exact path="/">
+              <ProductList />
+            </Route>
+            <BottomLinks />
+            <Footer />
+          </Route>
+        </Switch>
       </div>
-      <ProductList />
-      <BottomLinks />
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
