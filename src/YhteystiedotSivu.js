d273fa8559be09486ccef05b5074d605a6583b0c
MSI\antti <n0suan00@students.oamk.fi>
2021-03-29 13:37:26 +0300

    -yhteystiedotSivu

---
+++ src/YhteystiedotSivu.js
@@ -0,0 +1,18 @@
+import Navigation from './components/navigation';
+import Footer from './components/Footer';
+import Tietoja from './components/Tietoja';
+
+
+
+function YhteystiedotSivu() {
+    return (
+        <div>
+            <Navigation />
+            <Footer />
+            <Tietoja />
+        </div>
+
+    );
+}
+
+export default YhteystiedotSivu;
