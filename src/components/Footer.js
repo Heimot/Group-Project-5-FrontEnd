645bf79942de7731e0c2873223b631faf2877108
Juho Vikman <c0viju00@students.oamk.fi>
2021-04-06 11:04:44 +0300

    footteriin sisältöä

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,34 @@ import React from 'react'
 import './footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 
 export default function Footer() {
    return (
       <footer className="bottomi container-fluid">
          <div className="row">
-         <div className="col text-center">
-               <p>2021</p>
-               <p>Ryhmä 5</p>
+         <div className="col">
+               <h4>Asiakaspalvelu</h4>
+               <p>Ma-Pe 9-16</p>
+               <p>+35840123456</p>
+               <p>digifemma@digifemma.fi</p>
+               <p>Digikatu 3</p>
+               <p>90100, Oulu</p>
             </div>
-            <div className="col text-center">
-               <a href="https://www.facebook.com/" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a> 
-               <a href="https://www.twitter.com/" target="_blank"><FontAwesomeIcon icon={faTwitter}/></a> 
+            <div className="col">
+               <h4>Info</h4>
+               <p>Yhteystiedot</p>
+               <p>Palvelut ja huolto</p>
+               <p>UKK</p>
+               <p>Yritysmyynti</p>
+               <p>Tietosuojaseloste</p>
+            </div>
+            <div className="col">
+               <a href="https://www.facebook.com/" target="_blank"><FontAwesomeIcon icon={faFacebook} size="6x" /></a> 
+               <a href="https://www.twitter.com/" target="_blank"><FontAwesomeIcon icon={faTwitter} size="6x"/></a>
+               <a href="https://www.youtube.com/" target="_blank"><FontAwesomeIcon icon={faYoutube} size="6x"/></a>
+               <a href="https://www.instagram.com/" target="_blank"><FontAwesomeIcon icon={faInstagram} size="6x"/></a>
             </div>
          </div>
       </footer>
