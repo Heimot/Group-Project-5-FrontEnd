0b5953397717fd06d924c1f71fd13b7b41e316cb
MSI\antti <n0suan00@students.oamk.fi>
2021-04-20 11:10:59 +0300

    -admin_page

--- src/components/AddUserManually.js
+++ src/components/AddUserManually.js
@@ -6,11 +6,13 @@ export default function AddUserManually() {
     const [serverResponse, setServerResponse] = useState([]); // shows last insert ID
 
 
+    
 
     const [customerid, setCustomerid] = useState(0);
     const [shipping, setShipping] = useState('');
 
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [firstname, setFirstname] = useState('');
     const [lastname, setLastname] = useState('');
     const [address, setAddress] = useState('');
@@ -19,41 +21,61 @@ export default function AddUserManually() {
     const [phone, setPhone] = useState('');
 
 
-    function insertOrder(e) {
+    function registration(e) {
         e.preventDefault();
-
-        if (shipping === '' || email === '' || firstname === '' || lastname === '' || address === '' || postalcode === '' || city === '' || phone === '') {
-            alert("Kaikki kentät vaaditaan!");
-            return;
+    
+        if (email === '' || password === '' || firstname === '' || lastname === '' || address === '' || postalcode === '' || city === '' || phone === '') {
+          alert("Kaikki kentät vaaditaan!");
+          return;
         }
-
-        fetch('http://localhost/Group-Project-5-BackEnd/neworder.php', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify({
-                customerid: customerid,
-                shipping: shipping,
-                email: email,
-                firstname: firstname,
-                lastname: lastname,
-                address: address,
-                postalcode: postalcode,
-                city: city,
-                phone: phone
-            })
+    
+        fetch('http://localhost/Group-Project-5-BackEnd/registration_check.php', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-type':'application/json',
+          },
+          body: JSON.stringify({
+            email: email,
+          })
         })
-            .then(response => response.json())
-            .then(
-                (response) => {
-                    setServerResponse(response);
-                }, (error) => {
-                    alert(error);
-                }
-            )
-    }
+        .then(response => response.text())
+        .then(
+          (response) => {
+            if (response === "Success!") {
+              fetch('http://localhost/Group-Project-5-BackEnd/registration.php', {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-type':'application/json',
+                },
+                body: JSON.stringify({
+                  email: email,
+                  password: password,
+                  firstname: firstname,
+                  lastname: lastname,
+                  address: address,
+                  postalcode: postalcode,
+                  city: city,
+                  phone: phone
+                 })
+              })
+              .then(response => response.text())
+              .then(
+              (response) => {
+                setServerResponse(response);
+              }, (error) => {
+                alert(error);
+              }
+              )
+            } else {
+              alert("Already registered!")
+            }
+          }, (error) => {
+            alert(error);
+          }
+        )
+      }
 
 
     return (
@@ -61,7 +83,7 @@ export default function AddUserManually() {
             <div className="row">
             <div className="col-sm-12 col-md-12 col-lg-12">
                 <h1 className="otsikkoLisUusTil">Lisää uusi asiakas</h1>
-                <form onSubmit={insertOrder}>
+                <form onSubmit={registration}>
 
                         <div className="ekatTiedot">
                             <input className="form-group col-md-6" value={firstname} onChange={e => setFirstname(e.target.value)} placeholder='Asiakkaan etunimi...' />
@@ -71,9 +93,10 @@ export default function AddUserManually() {
                         </div>
 
                         <div className="tokatTiedot">
-                            <input className="form-group col-md-4" value={email} onChange={e => setEmail(e.target.value)} placeholder='Asiakkaan sähköposti...' />
-                            <input className="form-group col-md-4" value={city} onChange={e => setCity(e.target.value)} placeholder='Asiakkaan postitoimipaikka...' />
-                            <input className="form-group col-md-4" value={phone} onChange={e => setPhone(e.target.value)} placeholder='Asiakkaan puhelinnumero...' />
+                            <input className="form-group col-md-6" value={email} onChange={e => setEmail(e.target.value)} placeholder='Asiakkaan sähköposti...' />
+                            <input className="form-group col-md-6" value={city} onChange={e => setCity(e.target.value)} placeholder='Asiakkaan postitoimipaikka...' />
+                            <input className="form-group col-md-6" value={phone} onChange={e => setPhone(e.target.value)} placeholder='Asiakkaan puhelinnumero...' />
+                            <input className="form-group col-md-6" value={password} onChange={e => setPassword(e.target.value)} placeholder='Luo salasana...'/>
                             <input type="hidden" value={customerid} onChange={e => setCustomerid(e.target.value)}/>
                         </div>
                             <button type="submit" className="tilausNappi btn btn-block">Lisää Asiakas:</button>
