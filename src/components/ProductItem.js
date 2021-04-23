f5dda0086aacb74c1f0a7c44ee960928eef91f5e
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-04-23 18:34:40 +0300

    Fixed cart.

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -8,38 +8,38 @@ export default function ProductItem(props) {
   const [open, setOpen] = useState(false);
 
   function addToCart(ids) {
-    let ID = ids.toString();
+    let id = ids.toString();
     let cart = [];
     if (localStorage.getItem("cart")) {
         cart = JSON.parse(localStorage.getItem("cart"));
     }
-    const found = cart.some(el => el.ID === ID)
+    const found = cart.some(el => el.id === id)
     if (found) {
-        const allCartValues = cart.filter(item => item.ID !== ID)
-        const cartValue = cart.filter(item => item.ID === ID)
-        allCartValues.push({ID, amount: cartValue[0].amount + 1 })
+        const allCartValues = cart.filter(item => item.id !== id)
+        const cartValue = cart.filter(item => item.id === id)
+        allCartValues.push({id, amount: cartValue[0].amount + 1 })
         cart = allCartValues;
     } else {
-        cart.push({ ID, amount: 1 })
+        cart.push({ id, amount: 1 })
     }
     let stringCart = JSON.stringify(cart);
     localStorage.setItem("cart", stringCart);
-    let name = props.product.tuote;
+    let name = props.product.name;
     // kun kuva haetaan backendistä niin tämä pitää muuttaa :)
     let IMG = "https://bulma.io/images/placeholders/128x128.png";
-    props.isOpen2({ID, name, IMG});
+    props.isOpen2({id, name, IMG});
 }
 
   return (
     <Card className="productCard" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
-    <a href={`/product?productid=${props.product.id}&product=${props.product.tuote}`}><Card.Img variant="top" src="https://bulma.io/images/placeholders/128x128.png" /></a>
-    <Card.Body>
-      <Card.Text className="text">
-        <a href={`/product?productid=${props.product.id}&product=${props.product.tuote}`}>
-        {props.product.tuote}
+    <a href={`/product?productid=${props.product.id}`}><Card.Img variant="top" src="https://bulma.io/images/placeholders/128x128.png" /></a>
+    <Card.Body className="productCardTitle">
+      <Card.Text className="text productCardTitle">
+        <a href={`/product?productid=${props.product.id}`}>
+        {props.product.name}
         </a>
       </Card.Text>
-      <Card.Title className="price">{props.product.hinta}</Card.Title>
+      <Card.Title className="price">{props.product.price}</Card.Title>
     </Card.Body>
     {open ? <Card className="moreData">
         <Button onClick={() => addToCart(props.product.id)} className="addToCart">Lisää ostoskoriin</Button>
