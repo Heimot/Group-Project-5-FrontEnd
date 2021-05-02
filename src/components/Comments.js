9e43e9cca8aead18df78c37f1df8d079233526b9
Santeri Korpinen <n0koja00@students.oamk.fi>
2021-05-03 02:19:24 +0300

    Kommenttiosiot, muutokset productpagelle

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,19 +1,66 @@
 import React from 'react'
+import useFetch from "./hooks/fetch";
+import Button from "react-bootstrap/Button";
+import {useState} from "react"
+import './comments.css'
 
 export default function Comments() {
-   // Kovakoodatut kommentit
-   let kommentit = ["Tämähän on aivan ihime tuote", "Aivan paras (best evö)s"]
-   
+   // Kommentin jättämiseen tarvittavat hookit
+   const [comment, setComment] = useState("")
+
+   // Tietokannasta kommentit
+   const urlParams = new URLSearchParams(window.location.search);
+   const myParam = urlParams.get("productid");
+   let comments = useFetch(
+     "http://localhost/Group-Project-5-BackEnd/comments.php?id=" + myParam,
+     { method: "GET", headers: { "Content-Type": "application/json" } } 
+   );
+   console.log(comments)
+
    function readComments() {
-      const printedComments = kommentit.map((kommentti) => <li key={kommentti}>{kommentti}</li>);
+      const printedComments = comments.map(function(kommentti) { return <li key={kommentti.id}>{kommentti.comments}</li>});
+      console.log(printedComments)
       return printedComments
+   }
+   // Kommenttien lisääminen
+ 
+   function postComment(e) {
+      e.preventDefault();
+      fetch ('http://localhost/Group-Project-5-BackEnd/postComment.php',
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-type':'application/json',
+          },
+          body: JSON.stringify({
+            productID: myParam,
+            text: comment,
+          }),
+          
+        }
+      )
+      .then((res) => res.json())
+      .then ((result) => {
+        console.log(result);
+      })
+      console.log(myParam);
+      console.log(comment)
    }
 
    return (
       <div>
-         <ul>
+         <form onSubmit={postComment}>
+         <input type="text" name="comment" value={comment} onChange={e => setComment(e.target.value)} />
+            <Button className="buttoni" type="submit">
+               Post comment
+            </Button>
+         </form>
+         {comments ?
+         <ul className="list">
             {readComments()}
          </ul>
+         : null }
       </div>
    )
 }
