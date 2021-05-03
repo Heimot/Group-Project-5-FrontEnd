dcdc030be2bb74171c480b7e39ade7b404889944
HeliotropicAfterglow <HeliotropicAfterglow@users.noreply.github.com>
2021-05-03 04:52:38 +0300

    admin page done

--- src/components/AddProductManually.js
+++ src/components/AddProductManually.js
@@ -186,7 +186,7 @@ export default function AddProductManually() {
             </button>
           </form>
         </div>
-        <div>{JSON.stringify(serverResponse)}</div>
+        <div>{/*JSON.stringify(serverResponse)*/}</div>
       </div>
     </div>
   );
